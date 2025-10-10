90a93b4b17708f931fb9561d759ca741fdb4fd27
Curtis Yungen <curtisyungen@gmail.com>
2025-10-09 21:34:03 -0400

    remove hawaiian import from Music

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -5,199 +5,191 @@ import DonateBtns from "../components/DonateBtns/donateBtns";
 import Logo from "../components/Logo/logo";
 import Thumb from "../images/logos/thumb.svg";
 
-import {
-  DEFAULT_SONG,
-  hawaiian,
-  originals,
-  covers,
-  collabs
-} from "../content/songs";
+import { DEFAULT_SONG, originals, covers, collabs } from "../content/songs";
 import "./Music.css";
 
 const thumbStyle = {
-  cursor: "pointer",
-  marginBottom: "-8px",
-  marginLeft: "5px",
-  maxHeight: "30px",
-  maxWidth: "100%"
+    cursor: "pointer",
+    marginBottom: "-8px",
+    marginLeft: "5px",
+    maxHeight: "30px",
+    maxWidth: "100%",
 };
 
 class Music extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: DEFAULT_SONG,
-      autoPlay: false,
-      askForTip: false,
-      screenWidth: null,
-      mobilize: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: DEFAULT_SONG,
+            autoPlay: false,
+            askForTip: false,
+            screenWidth: null,
+            mobilize: false,
+        };
+    }
+
+    componentDidMount = () => {
+        if (window.location.protocol !== "https:") {
+            window.location.href =
+                "https:" +
+                window.location.href.substring(window.location.protocol.length);
+        }
+
+        window.addEventListener("resize", this.getScreenWidth.bind(this));
+        window.addEventListener("scroll", this.toggleMobilizer.bind(this));
+
+        this.getScreenWidth();
     };
-  }
 
-  componentDidMount = () => {
-    if (window.location.protocol !== 'https:') {
-        window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-    }
-    
-    window.addEventListener("resize", this.getScreenWidth.bind(this));
-    window.addEventListener("scroll", this.toggleMobilizer.bind(this));
+    getScreenWidth = () => {
+        let screenWidth =
+            window.innerWidth ||
+            document.documentElement.clientWidth ||
+            document.body.clientWidth;
 
-    this.getScreenWidth();
-  };
+        this.setState(
+            {
+                screenWidth,
+            },
+            () => {
+                this.toggleMobilizer();
+            }
+        );
+    };
 
-  getScreenWidth = () => {
-    let screenWidth =
-      window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth;
+    toggleMobilizer = () => {
+        let { screenWidth, mobilize } = this.state;
 
-    this.setState(
-      {
-        screenWidth
-      },
-      () => {
-        this.toggleMobilizer();
-      }
-    );
-  };
+        let audio = document
+            .getElementById("audio-section")
+            .getBoundingClientRect();
 
-  toggleMobilizer = () => {
-    let { screenWidth, mobilize } = this.state;
+        let scrollY = window.scrollY;
+        let diffY = scrollY - audio.y;
 
-    let audio = document
-      .getElementById("audio-section")
-      .getBoundingClientRect();
+        if (screenWidth >= 768) {
+            mobilize = false;
+        } else if (screenWidth < 768 && diffY > 750) {
+            mobilize = true;
+        } else {
+            mobilize = false;
+        }
 
-    let scrollY = window.scrollY;
-    let diffY = scrollY - audio.y;
+        this.setState({
+            mobilize,
+        });
+    };
 
-    if (screenWidth >= 768) {
-      mobilize = false;
-    } else if (screenWidth < 768 && diffY > 750) {
-      mobilize = true;
-    } else {
-      mobilize = false;
-    }
+    toggleAutoPlay = () => {
+        this.setState({
+            autoPlay: !this.state.autoPlay,
+        });
+    };
 
-    this.setState({
-      mobilize
-    });
-  };
+    toggleTip = () => {
+        this.setState({
+            askForTip: true,
+        });
+    };
 
-  toggleAutoPlay = () => {
-    this.setState({
-      autoPlay: !this.state.autoPlay
-    });
-  };
+    setTitle = (title) => {
+        this.setState({
+            title,
+        });
+    };
 
-  toggleTip = () => {
-    this.setState({
-      askForTip: true
-    });
-  };
+    render() {
+        const { title, autoPlay, askForTip, mobilize } = this.state;
+        return (
+            <div className="container music-container">
+                <div className="row row-style justify-content-center text-center">
+                    <p className="title large-title">MUSIC</p>
+                    <p className="description text-left">
+                        Music has been in my life thanks to my mother and
+                        father. There was always music in my home, either with
+                        singing or just music on records or the radio. Dad liked
+                        big band music and quirky, novelty music, from
+                        performers such as Spike Jones and Stan Freberg. Mom
+                        loved old time hymns and church choral music with vocal
+                        harmonies.
+                    </p>
+                    <div className="spacer"></div>
+                </div>
 
-  setTitle = title => {
-    this.setState({
-      title
-    });
-  };
+                <div className={`row placeholder-${mobilize}`} />
+                <div className={`mobilize  mobilize-${mobilize}`}>
+                    <div
+                        id="audio-section"
+                        className={`row row-style justify-content-center text-center audio-section`}
+                    >
+                        <div className="autoPlaySwitch">
+                            <p className="autoPlayLabel">AUTOPLAY</p>
+                            <label className="switch">
+                                <input
+                                    type="checkbox"
+                                    onChange={this.toggleAutoPlay}
+                                />
+                                <span className="slider round" />
+                            </label>
+                        </div>
+                        <AudioPlayer title={title} autoPlay={autoPlay} />
+                    </div>
 
-  render() {
-    const { title, autoPlay, askForTip, mobilize } = this.state;
-    return (
-      <div className="container music-container">
-        <div className="row row-style justify-content-center text-center">
-          <p className="title large-title">MUSIC</p>
-          <p className="description text-left">
-            Music has been in my life thanks to my mother and father. There was
-            always music in my home, either with singing or just music on
-            records or the radio. Dad liked big band music and quirky, novelty
-            music, from performers such as Spike Jones and Stan Freberg. Mom
-            loved old time hymns and church choral music with vocal harmonies.
-          </p>
-          <div className="spacer"></div>
-        </div>
+                    <div className="donatePrompt text-center">
+                        {!askForTip ? (
+                            <div
+                                className="likeThisSong"
+                                onClick={this.toggleTip}
+                            >
+                                <Logo image={Thumb} style={thumbStyle} />
+                                Like this song?
+                            </div>
+                        ) : (
+                            <div className="leaveATip fadeIn-fast">
+                                <p className="description">Leave a tip!</p>
+                                <DonateBtns />
+                            </div>
+                        )}
+                    </div>
+                </div>
 
-        <div className={`row placeholder-${mobilize}`} />
-        <div className={`mobilize  mobilize-${mobilize}`}>
-          <div
-            id="audio-section"
-            className={`row row-style justify-content-center text-center audio-section`}
-          >
-            <div className="autoPlaySwitch">
-              <p className="autoPlayLabel">AUTOPLAY</p>
-              <label className="switch">
-                <input type="checkbox" onChange={this.toggleAutoPlay} />
-                <span className="slider round" />
-              </label>
+                <div className="row row-style justify-content-center text-center songListRow">
+                    {Object.entries(originals).length > 0 ? (
+                        <SongList
+                            listTitle={"Originals"}
+                            list={originals}
+                            selectedTitle={title}
+                            setTitle={this.setTitle}
+                        />
+                    ) : (
+                        <></>
+                    )}
+
+                    {Object.entries(covers).length > 0 ? (
+                        <SongList
+                            listTitle={"Covers"}
+                            list={covers}
+                            selectedTitle={title}
+                            setTitle={this.setTitle}
+                        />
+                    ) : (
+                        <></>
+                    )}
+
+                    {Object.entries(collabs).length > 0 ? (
+                        <SongList
+                            listTitle={"Collaborations"}
+                            list={collabs}
+                            selectedTitle={title}
+                            setTitle={this.setTitle}
+                        />
+                    ) : (
+                        <></>
+                    )}
+                </div>
             </div>
-            <AudioPlayer title={title} autoPlay={autoPlay} />
-          </div>
-
-          <div className="donatePrompt text-center">
-            {!askForTip ? (
-              <div className="likeThisSong" onClick={this.toggleTip}>
-                <Logo image={Thumb} style={thumbStyle} />
-                Like this song?
-              </div>
-            ) : (
-              <div className="leaveATip fadeIn-fast">
-                <p className="description">Leave a tip!</p>
-                <DonateBtns />
-              </div>
-            )}
-          </div>
-        </div>
-
-        <div className="row row-style justify-content-center text-center songListRow">
-
-        {Object.entries(originals).length > 0 ? (
-          <SongList
-            listTitle={"Originals"}
-            list={originals}
-            selectedTitle={title}
-            setTitle={this.setTitle}
-          />
-        ) : (
-            <></>
-        )}
-
-         {Object.entries(hawaiian).length > 0 ? (
-          <SongList
-            listTitle={"Hawaiian"}
-            list={hawaiian}
-            selectedTitle={title}
-            setTitle={this.setTitle}
-          />
-        ) : (
-            <></>
-        )}
-
-        {Object.entries(covers).length > 0 ? (
-          <SongList
-            listTitle={"Covers"}
-            list={covers}
-            selectedTitle={title}
-            setTitle={this.setTitle}
-          />
-                  ) : (
-            <></>
-        )}
-
-        {Object.entries(collabs).length > 0 ? (
-          <SongList
-            listTitle={"Collaborations"}
-            list={collabs}
-            selectedTitle={title}
-            setTitle={this.setTitle}
-          />
-                  ) : (
-            <></>
-        )}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Music;
